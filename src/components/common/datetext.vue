8ce7e0913362c36a26d809f91b7c32891a0c2f43
hechengjin <he_chengjin@outlook.com>
2020-12-28 23:25:30 +0800

    增加漏提交的文件

---
+++ src/components/common/datetext.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="date-text">{{ day }} <br />{{ hour }}</div>
+</template>
+<script>
+export default {
+  props: ['date'],
+  computed: {
+    day() {
+      if (this.date === null) {
+        return '';
+      }
+      return this.date.split(' ')[0];
+    },
+    hour() {
+      if (this.date === null) {
+        return '';
+      }
+      return this.date.split(' ')[1];
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.date-text {
+  width: 100px;
+  height: auto;
+  text-align: center;
+}
+</style>
---
+++ src/components/common/help-icon.vue
@@ -0,0 +1,8 @@
+<template>
+  <i class="h-icon-help" v-tooltip trigger="click" :content="text"></i>
+</template>
+<script>
+export default {
+  props: ['text']
+};
+</script>
